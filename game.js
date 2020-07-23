ee28a4d42a992ca239cfdd4924f6700c0146809f
ak1103dev <ak.apichan1103@gmail.com>
2020-07-23 19:03:34 +0700

    :construction: can click fullscreen with button

--- game.js
+++ game.js
@@ -3,8 +3,13 @@
 
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    parent: 'phaser-example',
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 600
+  },
   physics: {
     default: 'arcade',
     arcade: {
@@ -36,6 +41,7 @@ function preload () {
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.spritesheet('fullscreen', 'assets/fullscreen.png', { frameWidth: 64, frameHeight: 64 });
 }
 
 function create () {
@@ -113,6 +119,17 @@ function create () {
   this.physics.add.overlap(player, stars, collectStar, null, this);
 
   this.physics.add.collider(player, bombs, hitBomb, null, this);
+
+  var button = this.add.image(800-16, 16, 'fullscreen', 0).setOrigin(1, 0).setInteractive();
+  button.on('pointerup', function () {
+    if (this.scale.isFullscreen) {
+      button.setFrame(0);
+      this.scale.stopFullscreen();
+    } else {
+      button.setFrame(1);
+      this.scale.startFullscreen();
+    }
+  }, this);
 }
 
 function update () {
